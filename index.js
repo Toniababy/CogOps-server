5fba4bb9c679b147c5e0cbcd4ada77bd4b7f7131
toniaebereonyeka@gmail.com <techniaebere@gmail.com>
2026-04-02 19:52:49 +0100

    made changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000", "https://cog-ops-website.vercel.app"],
+  origin: ["http://localhost:3000", "https://cog-ops-website-one.vercel.app/"],
   credentials: true
 }));
 
